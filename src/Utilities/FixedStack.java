edc559f5688175ccf55c1f37b7652540f4b79b99
Vladimir Romanov <vladromweb@gmail.com>
2016-07-03 20:39:18 -0700

    FixedStack implementation
    FixedStack implementation

--- src/Utilities/FixedStack.java
+++ src/Utilities/FixedStack.java
@@ -1,5 +1,37 @@
 package Utilities;
+import InterfacesVR.IntStack;
 
-public class FixedStack {
+public class FixedStack implements IntStack {
+	private int stack[];
+	private int position;
+	
+	FixedStack(int size){
+		stack = new int[size];
+		position = 0;
+	}
+
+	//Pushing an item onto the stack!
+	@Override
+	public void push(int item) {
+		// TODO Auto-generated method stub
+		if(position >= stack.length-1){
+			System.out.println("Stack is full!");
+		}else{
+			stack[position] = item;
+			position++;
+		}
+	}
+	
+	//Popping the top item from the stack.
+	@Override
+	public int pop() {
+		// TODO Auto-generated method stub
+		if(position < 0){
+			System.out.println("Stack is empty!");
+			return 0;
+		}else{
+			return stack[position--];
+		}
+	}
 
 }
