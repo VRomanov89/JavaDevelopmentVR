ba51c83b73104ef0fd1fa00a1642e20b0c1b7f93
Vladimir Romanov <vladromweb@gmail.com>
2016-07-06 16:13:26 -0700

    Basic input implementation
    Basic input implementation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 //import InterfacesVR.IntStack;
 import Utilities.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String args[]) {
-		ColorList colors;
-		System.out.println(ColorList.Blue.getWeight());
+	public static void main(String args[]) throws IOException {
+		char c;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter characters. Use q to exit");
+		do {
+			c = (char) br.read();
+			System.out.println(c);
+		} while (c != 'q');
 	}
 }
