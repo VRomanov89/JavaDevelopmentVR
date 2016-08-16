011d254942acb72a4f939e63cac9a5e84af50bea
Vladimir Romanov <vladromweb@gmail.com>
2016-08-15 20:22:52 -0700

    Added tree implementation
    Added tree implementation

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import DataStructures.*;
 
 public class Main {
 	public static void main(String args[]){
+		BinaryTree theTree = new BinaryTree();
+		theTree.addNode(50, "Boss");
+		theTree.addNode(25, "VicePres");
+		theTree.addNode(56, "Manager");
+		theTree.addNode(24, "Sales");
+		theTree.addNode(53, "Eng");
+		theTree.addNode(234, "VicePres");
+		theTree.addNode(26, "Boss");
+		theTree.addNode(13, "VicePres");
+		theTree.inOrderTraverseTree(theTree.root);
 		
 	}
 }
